01bd52b0669bc6b7d6c78ecb639e358eac5e83ad
anantlumiq <anant.sirohi@lumiq.ai>
2021-02-26 16:45:13 +0530

    anant update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
 public static void main(String []args){
-Syste.out.println("Hello , world!");
+System.out.println("Hello , world!");
+System.out.println("Hello , anant");
+
 }
 }
